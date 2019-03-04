6ec3a6b5506fa8587fdd3fe96783179a4cdaaa9b
DavidDBaker <daviiddamiianbaker@gmail.com>
2019-03-04 13:49:40 -0300

    fiished

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const api = ( () =>{
-    const BASE_URL = 'https://thinkful-list-api.herokuapp.com/fgdl';
-    const getItems = function(){
-      return Promise.resolve('A successful response!');
-    };
-    return {
-      getItems: getItems
-    };
-  }();
+const api = (() =>{
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/dbqb';
+  const getItems = function(){
+    return Promise.resolve('A successful response!');
+  };
+  return {
+    getItems: getItems
+  };
+})();
